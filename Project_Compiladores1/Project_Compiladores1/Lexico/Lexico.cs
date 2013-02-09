96679d4f1629c21432bbf15f4dab5f1aab314e63
Diego Turcios <diegoturciostc@gmail.com>
2013-02-09 14:19:11 -0600

    Lexico

---
+++ Project_Compiladores1/Project_Compiladores1/Lexico/Lexico.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_Compiladores1.Lexico
+{
+    public enum TipoToken
+    {
+        TK_ID,
+        TK_INT,
+        TK_FLOAT,
+        TK_STRING,
+        TK_CHAR,
+        TK_BOOL,
+        TK_ARRAY,
+        TK_ASSIGN,
+        TK_IF,
+        TK_ELSE,
+        TK_WHILE,
+        TK_FOR,
+        TK_DO,
+        TK_TO,
+        TK_SWITCH,
+        TK_CASE,
+        TK_BREAK,
+        TK_FINSENTENCIA,
+        TK_OPENPAR,
+        TK_CLOSEPAR,
+        TK_OPENLLAVE,
+        TK_CLOSELLAVE,
+        TK_OPENCOR,
+        TK_CLOSECOR,
+        TK_PUNTO,
+        TK_COMA,
+        TK_DOSPUNTOS,
+        TK_SUMA,
+        TK_RESTA,
+        TK_MULT,
+        TK_DIVISION,
+        TK_MOD,
+        TK_DIV,
+        TK_IGUALDAD,
+        TK_DISTINTO,
+        TK_MENORQUE,
+        TK_MAYORQUE,
+        TK_MENORIGUAL,
+        TK_MAYORIGUAL,
+        TK_AND,
+        TK_OR,
+        TK_NOT,
+        TK_MASMAS,
+        TK_MENOSMENOS,
+        TK_MASIGUAL,
+        TK_PORIGUAL,
+        TK_ENTREIGUAL,
+        TK_MENOSIGUAL,
+        TK_CLASS,
+        TK_VOID,
+        TK_STRUCT,
+        TK_TRUE,
+        TK_FALSE,
+        TK_FINFLUJO,
+        TK_PRINT,
+        TK_READ,
+        TK_STRING_LIT,
+        TK_CHAR_LIT,
+        TK_THEN,
+        TK_PROGRAM,
+        TK_TYPE,
+        TK_VAR,
+        TK_BEGIN,
+        TK_END,
+        TK_FUNCTION,
+        TK_OF
+    }
+
+    public class Token
+    {
+        public string Lexema;
+        public TipoToken Tipo;
+        public int Linea;
+        public int Columna;
+
+        public Token(string Lex, TipoToken Tip, int Lin, int Col)
+        {
+            Lexema = Lex;
+            Tipo = Tip;
+            Linea = Lin;
+            Columna = Col;
+        }
+
+    }
+
+    public class Lexico
+    {
+        public string Contenido;
+        public int CurrentSymbol;
+        public int Col;
+        public int Fil;
+        public Dictionary<string, TipoToken> PalabrasReservadas;
+        public Dictionary<string, TipoToken> Operadores;
+        public char symbol;
+
+        public Lexico(string Cont)
+        {
+            Contenido = Cont;
+            CurrentSymbol = 0;
+            Col = 0;
+            Fil = 0;
+            PalabrasReservadas = new Dictionary<string, TipoToken>();
+            Operadores = new Dictionary<string, TipoToken>();           
+        }
+
+
+        public char nextSymbol()
+        {
+            if (CurrentSymbol >= Contenido.Length)
+                return (char)0;
+            char c = Contenido[CurrentSymbol];
+            if (c == '\n' || c == '\r')
+            {
+                Fil++;
+                Col = 0;
+            }
+            else
+            {
+                Col++;
+            }
+            CurrentSymbol++;
+            return c;
+        }
+
+        public Token NextToken()
+        {
+            int estado = 0;            
+            string lexema = "";
+            while (true)
+            {
+                switch (estado)
+                {
+                    case 0:
+                        if (char.IsDigit(symbol))
+                        {
+                            lexema += symbol;
+                            symbol = nextSymbol();
+                            estado = 3;
+                        }
+                        else if (char.IsLetter(symbol))
+                        {
+                            lexema += symbol;
+                            symbol = nextSymbol();
+                            estado = 1;
+                        }
+                        else if (Operadores.ContainsKey(symbol.ToString()) || symbol == '"' || symbol == '\'')
+                        {
+                            lexema += symbol;
+                            symbol = nextSymbol();
+                            estado = 8;
+                        }
+                        else if (char.IsWhiteSpace(symbol) || symbol == '\n' || symbol == '\r')
+                            symbol = nextSymbol();
+                        else if (symbol == '\0')
+                            return new Token("", TipoToken.TK_FINFLUJO, Fil, Col);
+                        else
+                        {
+                            throw new Exception("Simbolo no reconocido");
+                        }
+                        break;
+                    case 1:
+                        if (char.IsLetterOrDigit(symbol) || symbol == '_')
+                        {
+                            lexema += symbol;
+                            symbol = nextSymbol();
+                            estado = 1;
+                        }
+                        else
+                        {
+                            estado = 2;
+                        }
+                        break;
+
+                    case 2:
+                        if (PalabrasReservadas.ContainsKey(lexema.ToLower()))
+                        {
+                            return new Token(lexema, PalabrasReservadas[lexema.ToLower()], Fil, Col);
+                        }
+                        else
+                        {
+                            return new Token(lexema, TipoToken.TK_ID, Fil, Col);
+                        }
+
+                    case 3:
+                        if (char.IsDigit(symbol))
+                        {
+                            lexema += symbol;
+                            symbol = nextSymbol();
+                            estado = 3;
+                        }
+                        else if (symbol == '.')
+                        {
+                            lexema += symbol;
+                            symbol = nextSymbol();
+                            estado = 4;
+                        }
+                        else
+                        {
+                            estado = 7;
+                        }
+                        break;
+
+                    case 4:
+                        if (char.IsDigit(symbol))
+                        {
+                            lexema += symbol;
+                            symbol = nextSymbol();
+                            estado = 5;
+                        }
+                        else
+                        {
+                            throw new Exception("Se esperaba un numero!!");
+                        }
+                        break;
+
+                    case 5:
+                        if (char.IsDigit(symbol))
+                        {
+                            lexema += symbol;
+                            symbol = nextSymbol();
+                            estado = 5;
+                        }
+                        else
+                        {
+                            estado = 6;
+                        }
+                        break;
+
+                    case 6:
+                        return new Token(lexema, TipoToken.TK_FLOAT, Fil, Col); //Aqui regresa un numero flotante
+
+                    case 7:
+                        return new Token(lexema, TipoToken.TK_INT, Fil, Col); //Aqui regresa un numero entero
+
+                    case 8:
+                        if (Operadores.ContainsKey(lexema.ToLower() + symbol.ToString().ToLower()))
+                        {
+                            lexema += symbol;
+                            symbol = nextSymbol();
+                            return new Token(lexema, Operadores[lexema], Fil, Col);
+                        }
+                        else if(lexema == "\"")
+                        {
+                            estado = 9;
+                        }
+                        else if (lexema == "'")
+                        {
+                            estado = 10;
+                        }
+                        else
+                            return new Token(lexema, Operadores[lexema], Fil, Col);
+                        break;
+                    case 9:
+                        if (symbol == '"')
+                        {
+                            lexema += symbol;
+                            symbol = nextSymbol();
+                            return new Token(lexema, TipoToken.TK_STRING_LIT, Fil, Col);
+                        }
+                        else
+                        {
+                            lexema += symbol;
+                            symbol = nextSymbol();
+                        }
+                        break;
+                    case 10:
+                        if (symbol == '\'')
+                        {
+                            lexema += symbol;
+                            symbol = nextSymbol();
+                            if (lexema.Length <= 3)
+                            {
+                                return new Token(lexema, TipoToken.TK_CHAR_LIT, Fil, Col);
+                            }
+                            else
+                            {
+                                throw new Exception("Simbolo no reconocido");
+                            }
+                        }
+                        else
+                        {
+                            lexema += symbol;
+                            symbol = nextSymbol();
+                        }
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ Project_Compiladores1/Project_Compiladores1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Project_Compiladores1.Lexico;
+
+
+namespace Project_Compiladores1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            StreamReader streamReader = new StreamReader("C:\\Pascal.txt");
+            string text = streamReader.ReadToEnd();
+            streamReader.Close();
+
+            /*LexicoC l = new LexicoC(text);
+            Token t = l.NextToken();
+            while (t.Tipo != TipoToken.TK_FINFLUJO)
+            {
+                Console.WriteLine(t.Tipo.ToString() + "  " + t.Lexema);
+                t = l.NextToken();
+            }
+            
+            LexicoJava lj = new LexicoJava(text);
+            Token tj = lj.NextToken();
+            while (tj.Tipo != TipoToken.TK_FINFLUJO)
+            {
+                Console.WriteLine(tj.Tipo.ToString() + "  " + tj.Lexema);
+                tj = lj.NextToken();
+            }*/
+
+            LexicoPascal lp = new LexicoPascal(text);
+            Token tp = lp.NextToken();
+            while (tp.Tipo != TipoToken.TK_FINFLUJO)
+            {
+                Console.WriteLine(tp.Tipo.ToString() + "  " + tp.Lexema);
+                tp = lp.NextToken();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
