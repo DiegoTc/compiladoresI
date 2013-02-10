0da4524d0fab2e13c89f596f99d413a7f9ebae53
Erick J. Viera <b195ex@unitec.edu>
2013-02-10 02:58:46 -0600

    asdf

--- Project_Compiladores1/Project_Compiladores1/Lexico/LexicoPascal.cs
+++ Project_Compiladores1/Project_Compiladores1/Lexico/LexicoPascal.cs
@@ -14,18 +14,18 @@ namespace Project_Compiladores1.Lexico
              Operadores.Add("-", TipoToken.TK_RESTA);
              Operadores.Add("*", TipoToken.TK_MULT);
              Operadores.Add("/", TipoToken.TK_DIVISION);            
-             Operadores.Add("==", TipoToken.TK_IGUALDAD);
+             Operadores.Add("=", TipoToken.TK_IGUALDAD);
              Operadores.Add("<>", TipoToken.TK_DISTINTO);
              Operadores.Add("<", TipoToken.TK_MENORQUE);
              Operadores.Add(">", TipoToken.TK_MAYORQUE);
              Operadores.Add("<=", TipoToken.TK_MENORIGUAL);
-             Operadores.Add(">=", TipoToken.TK_MAYORIGUAL);             
+             Operadores.Add(">=", TipoToken.TK_MAYORIGUAL);             /*
              Operadores.Add("++", TipoToken.TK_MASMAS);
              Operadores.Add("--", TipoToken.TK_MENOSMENOS);
              Operadores.Add("+=", TipoToken.TK_MASIGUAL);
              Operadores.Add("*=", TipoToken.TK_PORIGUAL);
              Operadores.Add("/=", TipoToken.TK_ENTREIGUAL);
-             Operadores.Add("-=", TipoToken.TK_MENOSIGUAL);
+             Operadores.Add("-=", TipoToken.TK_MENOSIGUAL);*/
              Operadores.Add(";", TipoToken.TK_FINSENTENCIA);
              Operadores.Add("(", TipoToken.TK_OPENPAR);
              Operadores.Add(")", TipoToken.TK_CLOSEPAR);
@@ -33,8 +33,8 @@ namespace Project_Compiladores1.Lexico
              Operadores.Add("]", TipoToken.TK_CLOSECOR);
              Operadores.Add("{", TipoToken.TK_OPENLLAVE);
              Operadores.Add("}", TipoToken.TK_CLOSELLAVE);
-             Operadores.Add("=", TipoToken.TK_ASSIGN);
-             Operadores.Add(".", TipoToken.TK_PUNTO);
+             Operadores.Add(":=", TipoToken.TK_ASSIGN);
+             //Operadores.Add(".", TipoToken.TK_PUNTO);
              Operadores.Add(",", TipoToken.TK_COMA);
              Operadores.Add(":", TipoToken.TK_DOSPUNTOS);
 
@@ -67,11 +67,6 @@ namespace Project_Compiladores1.Lexico
              PalabrasReservadas.Add("of", TipoToken.TK_OF);
              PalabrasReservadas.Add("to", TipoToken.TK_TO);
              PalabrasReservadas.Add("mod", TipoToken.TK_MOD);
-
-
-
-
-
 
              symbol = nextSymbol();
          }
--- Project_Compiladores1/Project_Compiladores1/Sintactico/pascalParser.cs
+++ Project_Compiladores1/Project_Compiladores1/Sintactico/pascalParser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Project_Compiladores1.Lexico;
 
 namespace Project_Compiladores1.Sintactico
 {
@@ -10,6 +11,52 @@ namespace Project_Compiladores1.Sintactico
         public pascalParser(Lexico.Lexico lexer)
             : base(lexer)
         {
+        }
+
+        public void parse()
+        {
+            SL();
+            if (currentToken.Tipo != TipoToken.TK_FINFLUJO)
+                throw new Exception("Se esperaba fin flujo ");
+        }
+
+        void SL()
+        {
+            S();
+
+        }
+
+        void S()
+        {
+            if (currentToken.Tipo == TipoToken.TK_ID)
+            {
+                currentToken = lex.NextToken();
+                expr();
+                if (currentToken.Tipo == TipoToken.TK_THEN)
+                {
+                    IFP();
+                }
+            }
+        }
+
+        void IFP()
+        {
+            CS();
+            ELSE();
+        }
+
+        void CS()
+        {
+        }
+
+        void ELSE()
+        {
+
+        }
+
+        void expr()
+        {
+
         }
     }
 }
