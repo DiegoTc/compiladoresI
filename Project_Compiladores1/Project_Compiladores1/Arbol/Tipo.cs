fb4f6acbad08aee7aec22cbf8e3e463bb00b2935
Christian Pineda <christianpineda@gmail.com>
2013-03-09 18:29:13 -0600

    STRUCT

--- Project_Compiladores1/Project_Compiladores1/Arbol/Tipo.cs
+++ Project_Compiladores1/Project_Compiladores1/Arbol/Tipo.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Project_Compiladores1.Arbol
@@ -70,16 +71,80 @@ namespace Project_Compiladores1.Arbol
 
     class Struct : Tipo
     {
+        private T_Campos _campos;
+
+        public Struct(T_Campos Camp)
+        {
+            Campos = Camp;
+        }
+
+        public T_Campos Campos
+        {
+            get { return _campos; }
+            set { _campos = value; }
+        }
+
         public override bool esEquivalente(Tipo t)
         {
             return t is Struct;
         }
     }
 
+    class Arreglo : Tipo
+    {
+        private ArrayList dimensiones; //Debe Ser Entero
+        private Tipo contenido;
+
+        public Tipo Contenido
+        {
+            get { return contenido; }
+            set { contenido = value; }
+        }
+
+        public ArrayList Dimensiones
+        {
+            get { return dimensiones; }
+            set { dimensiones = value; }
+        }
+
+
+        public override bool esEquivalente(Tipo t)
+        {
+            return t is Arreglo;
+        }
+
+    }
 
     class T_Campos : Dictionary<string, Tipo>
     {
-        ArrayList ids= new ArrayList();
+        private ArrayList ids = new ArrayList();
+
+        public ArrayList Ids
+        {
+            get { return ids; }
+            set { ids = value; }
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public new void Add(String key, Tipo value)
+        {
+            ids.Add(key);
+            base.Add(key, value);            
+
+        }
+
+        public void addVars(Object[] p)
+        {
+            ArrayList Ids = ((ArrayList) p[0]);
+            Tipo t = ((Tipo) p[1]);
+            for (int i = 0; i < ids.Count; i++)
+            {
+                this.Add(Ids[i].ToString(), t);
+
+            }
+        }
     }
+
+
 
 }
